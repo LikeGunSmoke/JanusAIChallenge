b4b157cef574032cfe228caea0d60d985bc2000f
Robert Strange <robertjohn23@live.com>
2022-05-03 05:50:08 -0600

    Implement strict typing of data arrays, modifiy listItems function

--- JanusAIChallenge/generateReport.ts
+++ JanusAIChallenge/generateReport.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 
-const generateReport = (data: any[], testName?: string) => {
+const generateReport = (data: {title: string, url: string, date: string}[], testName?: string) => {
 
   let csv: string = "";
   for (let i = 0; i < 3; i++) {
     let currentItem = data[i];
-    csv += `"${currentItem.title}", "${currentItem.url}", "${currentItem.date}",` + '\n'
+    csv += `"${currentItem.title}", "https://amazon.com/${currentItem.url}", "${currentItem.date}",` + '\n'
   };
 
   fs.writeFile(`${testName}_BestPrice.csv`, csv, 'utf8', (err) => {
--- JanusAIChallenge/models/SearchAmazon.ts
+++ JanusAIChallenge/models/SearchAmazon.ts
@@ -4,12 +4,12 @@ export class SearchAmazon {
 
   page: Page;
   searchTermInput: Locator;
-  data: any[];
   date: string;
   title: string;
   url: string;
   price: number;
-  sortedData: any[];
+  data: {title: string, url: string, date: string, price: number}[];
+  sortedData: {title: string, url: string, date: string, price: number}[];
 
   constructor(page: Page) {
     this.page = page;
@@ -25,14 +25,15 @@ export class SearchAmazon {
     await Promise.all([
       this.page.waitForNavigation(),
       this.page.locator('[aria-label="Search"]').press('Enter')
-  ]);
+    ]);
   }
 
   public async listItems() {
-    const listings = await this.page.$$eval('.s-card-container', items => {
+
+    const listings = await this.page.evaluate(() => {
+      const items: NodeListOf<Element> = document.querySelectorAll('.s-card-container');
       this.data = [];
       items.forEach((item) => {
-        // listing may be displayed without price, only include purchasable listings
         if (item.querySelector('.a-offscreen') !== null) {
           let date = new Date().toLocaleDateString();
           let title = item.querySelector('h2').innerText;
@@ -44,7 +45,7 @@ export class SearchAmazon {
       this.sortedData = this.data.sort((a,b) => a.price-b.price);
       return this.sortedData;
     });
-    return listings
+    return listings;
   };
 
 };
